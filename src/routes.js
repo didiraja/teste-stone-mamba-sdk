bbfb55d3cfb4616160c01b0e4112e4b91a5dc379
Dico Didiraja <didiraja@hotmail.com>
2019-04-28 17:17:24 -0300

    capturando api de star wars

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import Home from './routes/Home.html';
-import About from './routes/About.html';
+import Login from './routes/Login.html';
 
 export default {
   '/': Home,
-  '/about': About,
+  '/login': Login,
 };
