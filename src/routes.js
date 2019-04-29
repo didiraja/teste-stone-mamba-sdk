03978719512badf8335ac3e444c3223a82479bed
Dico Didiraja <didiraja@hotmail.com>
2019-04-28 22:50:04 -0300

    criada página de Log com todos os usuários

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Home from './routes/Home.html';
 import Login from './routes/Login.html';
+import Log from './routes/Log.html';
 
 export default {
   '/': Home,
   '/login': Login,
+  '/log': Log,
 };
